cf9b335eecc233a98924d91ea245d6a3644a5479
rocky <rocky.luo@outlook.com>
2019-01-16 08:36:13 +0800

    init

---
+++ jrp-server/src/main/java/com/rocky/jrp/server/TimeServer.java
@@ -0,0 +1,40 @@
+package com.rocky.jrp.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+    public void bind(int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+
+        }catch (Exception e) {
+            System.out.println(e);
+        }finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel ch) throws Exception {
+            ch.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) {
+        new TimeServer().bind(8080);
+    }
+}
